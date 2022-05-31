cc93e87e411e7fc6ffcca6f4280379b499007e73
jéssica senger <jessicasbogoni1@gmail.com>
2022-05-31 13:56:25 -0300

    get posts from prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,6 @@
+import { GetStaticProps } from 'next'
+import { getPrismicClient } from '../../services/prismic'
+import Prismic from '@prismicio/client'
 import Head from 'next/head'
 import styles from './styles.module.scss'
 
@@ -29,4 +32,21 @@ export default function Posts() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  return {
+    props: {
+
+    }
+  }
 }
